464d62628a1d04d43a239f863ce7e83d9d2549c1
PG_LoLo <novgosh@gmail.com>
2017-06-05 12:40:04 +0300

    some calculations

--- src/main.cpp
+++ src/main.cpp
@@ -378,16 +378,19 @@ void shs()
 
     auto projMatrix = makeRandomMatrix(2, linearHessian.nDims);
 
-    for (size_t i = 1; i < cnt; i++) {
+    for (size_t i = 0; i < cnt; i++) {
         auto direction = readVect(input);
         LOG_INFO("Path #{}. Initial direction: {}", i, direction.transpose());
+
+        if (i < 2)
+            continue;
 
         system(str(boost::format("mkdir %1%") % i).c_str());
         double value = linearHessian(direction);
 
         vector<double> xs, ys;
 
-        for (size_t j = 0; j < 200; j++) {
+        for (size_t j = 0; j < 600; j++) {
             vect proj = projMatrix * direction / direction.norm();
             xs.push_back(proj(0)), ys.push_back(proj(1));
 
@@ -395,31 +398,37 @@ void shs()
 
             vect prev = direction;
             direction = direction / direction.norm() * r;
-            auto path = optimizeOnSphere(makeHistoryStrategy(StopStrategy(5e-4, 5e-4)), linearHessian, direction, r, 0);
-            if (path.empty()) {
-                LOG_ERROR("empty path");
+            try {
+                auto path = optimizeOnSphere(makeHistoryStrategy(StopStrategy(5e-4, 5e-4)), linearHessian, direction, r, 0);
+
+                if (path.empty()) {
+                    LOG_ERROR("empty path");
+                    break;
+                }
+                direction = path.back();
+
+                double newValue = linearHessian(direction);
+                LOG_INFO("New {} point in path:\n\tvalue = {:.13f}\n\tdelta norm = {:.13f}\n\t{}\nchemcraft coords:\n{}", j,
+                         newValue, (direction / direction.norm() - prev / prev.norm()).norm(), direction.transpose(),
+                         toChemcraftCoords(charges, linearHessian.fullTransform(direction)));
+
+                ofstream output(str(boost::format("./%1%/%2%.xyz") % i % j));
+                output << toChemcraftCoords(charges, linearHessian.fullTransform(direction)) << endl;
+
+                if (newValue < value) {
+                    LOG_ERROR("newValue < value [{:.13f} < {:.13f}]. Stopping", newValue, value);
+    //                break;
+                }
+
+                value = newValue;
+            }
+            catch (GaussianException const& exc) {
                 break;
             }
-            direction = path.back();
-
-            double newValue = linearHessian(direction);
-            LOG_INFO("New {} point in path:\n\tvalue = {:.13f}\n\tdelta norm = {:.13f}\n\t{}\nchemcraft coords:\n{}", j,
-                     newValue, (direction / direction.norm() - prev / prev.norm()).norm(), direction.transpose(),
-                     toChemcraftCoords(charges, linearHessian.fullTransform(direction)));
-
-            ofstream output(str(boost::format("./%1%/%2%.xyz") % i % j));
-            output << toChemcraftCoords(charges, linearHessian.fullTransform(direction)) << endl;
-
-            if (newValue < value) {
-                LOG_ERROR("newValue < value [{:.13f} < {:.13f}]. Stopping", newValue, value);
-//                break;
-            }
-
-            value = newValue;
         }
 
         framework.plot(framework.newPlot(), xs, ys);
-        return;
+//        return;
     }
 }
 
@@ -433,7 +442,7 @@ vector<vect> fromCartesianToPositions(vect v)
     return rs;
 }
 
-vect massCenter(vector<vect> const& rs)
+vect centerOfMass(vector<vect> const& rs)
 {
     auto p = makeConstantVect(3, 0);
     for (auto r : rs)
@@ -441,7 +450,7 @@ vect massCenter(vector<vect> const& rs)
     return p;
 }
 
-matrix inertyTensor(vector<vect> const& rs)
+matrix tensorOfInertia(vector<vect> const& rs)
 {
     auto J = makeConstantMatrix(3, 3, 0);
     for (auto r : rs)
@@ -461,8 +470,8 @@ vect solveEquations(vect v)
     v = withZeros;
 
     auto rs = fromCartesianToPositions(v);
-    auto p = massCenter(rs);
-    auto J = inertyTensor(rs);
+    auto p = centerOfMass(rs);
+    auto J = tensorOfInertia(rs);
 
     LOG_INFO("before:\np: {}\nJ:\n{}", p.transpose(), J);
 
@@ -507,8 +516,8 @@ vect solveEquations(vect v)
     }
 
     rs = fromCartesianToPositions(v);
-    p = massCenter(rs);
-    J = inertyTensor(rs);
+    p = centerOfMass(rs);
+    J = tensorOfInertia(rs);
     LOG_INFO("after:\np: {}\nJ:\n{}", p.transpose(), J);
     LOG_INFO("result: {}", v.transpose());
 
@@ -518,16 +527,16 @@ vect solveEquations(vect v)
 int main()
 {
     initializeLogger();
+//    shs();
+//    return 0;
 
 //    auto v = makeRandomVect(9);
 //    auto rs = fromCartesianToPositions(v);
 //
 //    LOG_INFO("\n{}", v.transpose());
-//    LOG_INFO("\n{}", massCenter(rs).transpose());
+//    LOG_INFO("\n{}", centerOfMass(rs).transpose());
 //    LOG_INFO("\n{}", inertyTensor(rs));
 
-    solveEquations(makeRandomVect(3));
-//
 //    ifstream input("./C2H4");
 //    vector<size_t> charges = readCharges(input);
 //    vect state = readVect(input);
@@ -558,35 +567,36 @@ int main()
 //    cout << d1.norm() << ' ' << d2.norm() << endl;
 
 //    filterPolarDirectionsLogFile();
-//    shs();
 
-//    vector<double> energies;
-//    vector<vect> states;
-//    for (size_t i = 0; i < 200; i++) {
-//        vector<size_t> charges;
-//        vect state;
-//        tie(charges, state) = readChemcraft(ifstream(str(boost::format("./0/%1%.xyz") % i)));
-//
-//        GaussianProducer molecule(charges);
-//        auto fixed = fixAtomSymmetry(molecule);
-//        state = fixed.backTransform(state);
-//
-//        auto hess = fixed.hess(state);
-//        auto grad = fixed.grad(state);
-//        auto energy = fixed(state);
-//
-//        energies.push_back(energy);
-//        states.push_back(state);
-//
-//        LOG_INFO("State #{}: {}\n\tenergy = {}\n\tgradient = {} [{}]\n\thess values = {}\nchemcraft coords:\n{}", i,
-//                 state.transpose(), energy, grad.norm(), grad.transpose(), singularValues(hess).transpose(),
-//                 toChemcraftCoords(charges, fixed.fullTransform(state)));
-//    }
-//
-//    for (size_t i = 0; i < energies.size(); i++) {
-//        LOG_INFO("#{}: {}, {}", i, i == 0 || energies[i - 1] < energies[i] ? '+' : '-',
-//                 i + 1 == energies.size() || energies[i] < energies[i + 1] ? '+' : '-');
-//    }
+    vector<double> energies;
+    vector<vect> states;
+    for (size_t i = 0; i < 200; i++) {
+        vector<size_t> charges;
+        vect state;
+        tie(charges, state) = readChemcraft(ifstream(str(boost::format("./2/%1%.xyz") % i)));
+
+        GaussianProducer molecule(charges);
+        auto fixed = fixAtomSymmetry(molecule);
+        state = fixed.backTransform(state);
+
+        auto hess = fixed.hess(state);
+        auto grad = fixed.grad(state);
+        auto energy = fixed(state);
+
+        energies.push_back(energy);
+        states.push_back(state);
+
+        LOG_INFO("State #{}: {}\n\tenergy = {}\n\tgradient = {} [{}]\n\thess values = {}\nchemcraft coords:\n{}", i,
+                 state.transpose(), energy, grad.norm(), grad.transpose(), singularValues(hess),
+                 toChemcraftCoords(charges, fixed.fullTransform(state)));
+    }
+
+    framework.plot(framework.newPlot(), energies);
+
+    for (size_t i = 0; i < energies.size(); i++) {
+        LOG_INFO("#{}: {}, {}", i, i == 0 || energies[i - 1] < energies[i] ? '+' : '-',
+                 i + 1 == energies.size() || energies[i] < energies[i + 1] ? '+' : '-');
+    }
 
 ////    findInitialPolarDirections(linearHessian, .3);
 }
